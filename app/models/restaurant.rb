133d2ea6ed8a1b823ea991fbae8a890db15481d4
Michael Kienbsuch <mkienbus@gmail.com>
2022-01-26 09:20:31 -0700

    initial migrations, start seed file

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,3 @@
 class Restaurant < ApplicationRecord
+    belong_to :user
 end
--- db/migrate/20220125232812_create_restaurants.rb
+++ db/migrate/20220125232812_create_restaurants.rb
@@ -1,6 +1,11 @@
 class CreateRestaurants < ActiveRecord::Migration[6.1]
   def change
     create_table :restaurants do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.string :name
+      t.string :address
+      t.string :cuisine_type
+      t.boolean :favorite
 
       t.timestamps
     end
