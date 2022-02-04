f0410a5a18b209d76d45754783ed2a2a8375d6db
Michael Kienbsuch <mkienbus@gmail.com>
2022-02-04 12:39:38 -0700

    added home link to About component

--- client/src/About.js
+++ client/src/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function About(){
 
@@ -6,6 +7,7 @@ function About(){
 
     return(
         <div>
+            <Link to={`/`}>Home</Link>
             <h2>Restaurant Tracking App</h2>
             <p>An application for tracking your favorite restaurants!</p>
         </div>
