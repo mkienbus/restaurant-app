ee93724339cf9f141ad8f9165a04185200ba6f55
Michael Kienbsuch <mkienbus@gmail.com>
2022-04-03 20:58:10 -0600

    code/comment cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,4 @@ class ApplicationController < ActionController::API
       @current_user = User.find(session[:user_id])
       render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
     end
-
-  
   end
